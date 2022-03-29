1db7de9ebe1660ea7c280b00efb45f091095eb8b
Zenzuzu <97756244+ZenxMato@users.noreply.github.com>
2022-03-29 07:22:51 +0700

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -212,7 +212,7 @@ async def choose_playmode(_, CallbackQuery):
             "This is not for you! Search You Own Song.", show_alert=True
         )
     buttons = choose_markup(videoid, duration, user_id)
-    photo=thumb,
+    photo="https://telegra.ph/file/21ce0a69c2c2eb31a45b0.jpg"
     picmetext = """
 **🎭 Silakan Pilih Jika Ingin Memutar.**
 **✨ Tersedia Music & Video.**
