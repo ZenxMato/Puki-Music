4d95621e6227c5fd35ca75d1797fd5373de8656b
Zenzuzu <97756244+ZenxMato@users.noreply.github.com>
2022-03-30 15:35:18 +0700

    Update others.py

--- Yukki/Inline/others.py
+++ Yukki/Inline/others.py
@@ -15,6 +15,8 @@ def others_markup(videoid, user_id):
             InlineKeyboardButton(text="⏭️", callback_data=f"skipcb"),
             InlineKeyboardButton(text="⏹️", callback_data=f"stopcb"),
             ),
+        ],
+        [
             InlineKeyboardButton(
                 text="ᴋᴇᴍʙᴀʟɪ",
                 callback_data=f"goback {videoid}|{user_id}",
