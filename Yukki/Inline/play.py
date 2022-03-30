1cd31d7cc3594ebdcbfeda255579eda1fb7aa902
Zenzuzu <97756244+ZenxMato@users.noreply.github.com>
2022-03-30 13:28:29 +0700

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -67,10 +67,6 @@ def url_markup2(videoid, duration, user_id):
             InlineKeyboardButton(
                 text="📤ᴅᴏᴡɴʟᴏᴀᴅ",
                 callback_data=f"qwertyuiopasdfghjkl {videoid}|{user_id}",
-            ),
-            InlineKeyboardButton(
-                text="ᴅᴏᴡɴʟᴏᴀᴅ",
-                callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
     ]
