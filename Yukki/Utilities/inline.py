7e645b646f1777ba85a23685e98edde7e7dc9618
Zenzuzu <97756244+ZenxMato@users.noreply.github.com>
2022-03-30 10:32:06 +0700

    Update inline.py

--- Yukki/Utilities/inline.py
+++ Yukki/Utilities/inline.py
@@ -69,15 +69,15 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
         ] + [
             (
                 EqInlineKeyboardButton(
-                    "❮",
+                    "⬅️",
                     callback_data="{}_prev({})".format(prefix, modulo_page),
                 ),
                 EqInlineKeyboardButton(
-                    "Close",
-                    callback_data="close",
+                    "🔃",
+                    callback_data="help_back",
                 ),
                 EqInlineKeyboardButton(
-                    "❯",
+                    "➡️",
                     callback_data="{}_next({})".format(prefix, modulo_page),
                 ),
             )
