fb751e496964acfa233da9ef9499dafacbfafce0
Zenzuzu <97756244+ZenxMato@users.noreply.github.com>
2022-04-04 02:42:53 +0700

    Update Converter.py

--- Yukki/Core/PyTgCalls/Converter.py
+++ Yukki/Core/PyTgCalls/Converter.py
@@ -17,18 +17,6 @@ async def convert(file_path: str) -> str:
         return out
     try:
         proc = await asyncio.create_subprocess_shell(
-            cmd=(
-                "ffmpeg "
-                "-y -i "
-                f"{file_path} "
-                "-f s16le "
-                "-ac 1 "
-                "-ar 48000 "
-                "-acodec "
-                "pcm_s16le "
-                f"{out}"
-            ),
-            stdin=asyncio.subprocess.PIPE,
             stderr=asyncio.subprocess.PIPE,
         )
 
